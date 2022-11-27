bb4badb90a577a8579ffac4cdb08f743481ba3b7
Hector Torrez <torrezhectorb@gmail.com>
2022-11-27 15:43:14 -0600

    add localstorage

--- main.js
+++ main.js
@@ -3,20 +3,47 @@ const menu = document.querySelector(".principal-nav");
 const openMenuBtn = document.querySelector(".btn-open");
 const openMenuBtn2 = document.querySelector(".btn-open-two");
 const closedMenuBtn = document.querySelector(".btn-closed");
-const tweet2 = document.querySelector(".content")
-const eliminated = document.querySelector('.eliminated')
-const body = document.querySelector('.body')
+const tweet2 = document.querySelector(".content");
+const eliminated = document.querySelector('.tieneClick');
+const body = document.querySelector('#body-remove');
+
 
 class Tweet {
 
     constructor(){
-        this.tweets = [];
+        // this.tweets = [];
+        this.loadLocalStorage()
+        
     }
 
     addTweet( sendTweet ){
-        this.tweets.push = (sendTweet);
+        this.tweets.push(sendTweet);
+        this.savedLocalStorage()
+        console.log(this.savedLocalStorage())
     }   
 
+    eliminatedTweets( id ) {
+        this.tweets = this.tweets.filter( sendTweet => sendTweet.id != id);
+        this.savedLocalStorage();
+    }
+
+    savedLocalStorage(){
+
+        localStorage.setItem( 'tweet', JSON.stringify(this.tweets)  );
+        
+
+    }
+
+    loadLocalStorage(){
+        if( localStorage.getItem('tweet')){
+            this.tweets = JSON.parse(localStorage.getItem('tweet'))
+        }else{
+            this.tweets = [];
+        }
+        
+    }
+
+    
     
 }
 
@@ -25,8 +52,12 @@ class _Tweet{
         this.message = message;
         this.hours = new Date().getHours();
         this.minutes = new Date().getMinutes();
+        this.id = new Date().getTime();
+        
+        
     }
 }
+
 
 
 
@@ -53,10 +84,9 @@ const todoTweet = new Tweet();
 
 
 
-
 const createTweet = ( tweet ) =>{
     const htmlTweet = `
-            <section class="body">
+            <section class="body" id="body-remove">
                 <div class="perfil-img ">
                     <img src="https://i.postimg.cc/501RGqx2/buff-cat-montreal-canada-1.png" alt="perfil-img ">
                 </div>
@@ -70,7 +100,7 @@ const createTweet = ( tweet ) =>{
                             <span>${tweet.hours}:${tweet.minutes}h</span>
                         </div>
 
-                        <div ><svg viewBox="0 0 24 24 " aria-hidden="true ""><g><circle cx="5 " cy="12 " r="2 "></circle><circle cx="12 " cy="12 " r="2 "></circle><circle cx="19
+                        <div ><svg data-id="${tweet.id}"  viewBox="0 0 24 24 " aria-hidden="true ""><g><circle cx="5 " cy="12 " r="2 "></circle><circle cx="12 " cy="12 " r="2 "></circle><circle cx="19
                             " cy="12 " r="2 "></circle></g></svg></div>
                     </div>
                     <div class="body-content-text ">
@@ -89,6 +119,8 @@ const createTweet = ( tweet ) =>{
 
     return div;
 }
+todoTweet.tweets.forEach( createTweet )
+
 txtInput.addEventListener('keyup', (event) => {
     if( event.keyCode === 13 && txtInput.value.length > 0 ){
         const nuevoTodo = new _Tweet( txtInput.value );
@@ -97,6 +129,29 @@ txtInput.addEventListener('keyup', (event) => {
         txtInput.value = '';
        
     }
+
+    
 });
+
+eliminated.addEventListener('click', (event) => {
+
+    
+    // const nombreElemento = event.target.localName;
+    const todoElemento = event.target.parentElement.parentElement;
+    const tweetId = todoElemento.getAttribute('data-id')
+    todoElemento.classList.toggle('.body')
+    // if( nombreElemento.includes(tweetId)){
+        todoTweet.eliminatedTweets( tweetId );
+        body.remove();
+        console.log(body)
+    // }
+
+    
+})
+
+
+
+
+
 
 
